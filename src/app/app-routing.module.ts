d02d1a586dda98d666a0b77e4a977a9a28887705
Mariana Kanashiro <m.kanashiro@iesb.edu.br>
2025-08-29 15:40:08 -0300

    deletados arquivos inutilizados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AlunosComponent } from './modules/alunos/alunos.component';
 import { AlunoFormComponent } from './modules/aluno-form/aluno-form.component';
-import { TreinosComponent } from './modules/treinos/treinos.component';
 import { TreinoFormComponent } from './modules/treino-form/treino-form.component';
 import { TipoTreinoFormComponent } from './modules/tipo-treino/tipo-treino.component';
 import { AlunoDetalheComponent } from './modules/aluno-detalhe/aluno-detalhe.component';
